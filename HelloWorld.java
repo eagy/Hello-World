5395da0d1f41c29301bfcd6f16e0fd046d1fc5ab
Daniel Eagy <daniel.eagy@gmail.com>
2016-03-31 08:03:45 -0700

    I adding a counting method to the program.

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,6 +4,15 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		System.out.println(counter(5));
+	}
+	
+	public static int counter(int n) {
+		if (n == 1)
+			return 1;
+		else
+			return n+counter(n-1);
 	}
 
 }
